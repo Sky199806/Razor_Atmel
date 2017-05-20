be072573763e460059504402299e1a464609d8f5
Sky199806 <771286048@qq.com>
2017-05-20 10:06:56 +0800

    Update 1.0

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -1,28 +1,14 @@
 /**********************************************************************************************************************
 File: user_app1.c                                                                
-zeyun wang
-----------------------------------------------------------------------------------------------------------------------
-To start a new task using this user_app1 as a template:
- 1. Copy both user_app1.c and user_app1.h to the Application directory
- 2. Rename the files yournewtaskname.c and yournewtaskname.h
- 3. Add yournewtaskname.c and yournewtaskname.h to the Application Include and Source groups in the IAR project
- 4. Use ctrl-h (make sure "Match Case" is checked) to find and replace all instances of "user_app1" with "yournewtaskname"
- 5. Use ctrl-h to find and replace all instances of "UserApp1" with "YourNewTaskName"
- 6. Use ctrl-h to find and replace all instances of "USER_APP1" with "YOUR_NEW_TASK_NAME"
- 7. Add a call to YourNewTaskNameInitialize() in the init section of main
- 8. Add a call to YourNewTaskNameRunActiveState() in the Super Loop section of main
- 9. Update yournewtaskname.h per the instructions at the top of yournewtaskname.h
-10. Delete this text (between the dashed lines) and update the Description below to describe your task
-----------------------------------------------------------------------------------------------------------------------
 
 Description:
-This is a user_app1.c file template 
+Provides a Tera-Term driven system to display, read and write an LED command list.
 
 ------------------------------------------------------------------------------------------------------------------------
 API:
 
 Public functions:
-
+None.
 
 Protected System functions:
 void UserApp1Initialize(void)
@@ -38,7 +24,7 @@ Runs current task state.  Should only be called once in main loop.
 
 /***********************************************************************************************************************
 Global variable definitions with scope across entire project.
-All Global variable names shall start with "G_UserApp1"
+All Global variable names shall start with "G_"
 ***********************************************************************************************************************/
 /* New variables */
 volatile u32 G_u32UserApp1Flags;                       /* Global state flags */
@@ -52,6 +38,9 @@ extern volatile u32 G_u32ApplicationFlags;             /* From main.c */
 extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
 extern volatile u32 G_u32SystemTime1s;                 /* From board-specific source file */
 
+extern u8 G_au8DebugScanfBuffer[DEBUG_SCANF_BUFFER_SIZE]; /* From debug.c */
+extern u8 G_u8DebugScanfCharCount;                        /* From debug.c */
+
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
@@ -59,15 +48,20 @@ Variable names shall start with "UserApp1_" and be declared as static.
 ***********************************************************************************************************************/
 static fnCode_type UserApp1_StateMachine;            /* The state machine function pointer */
 //static u32 UserApp1_u32Timeout;                      /* Timeout counter used across states */
-static u8 UserApp1_u8FinalCode[10]=0;
+
 
 /**********************************************************************************************************************
 Function Definitions
 **********************************************************************************************************************/
 
 /*--------------------------------------------------------------------------------------------------------------------*/
+/* Public functions                                                                                                   */
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Protected functions                                                                                                */
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------------------------------------------------
 Function: UserApp1Initialize
@@ -83,31 +77,23 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
-   
-   
-    If good initialization, set state to Idle 
+  u8 au8UserApp1Start1[] = "LED program task started\n\r";
+  
+  /* Turn off the Debug task command processor and announce the task is ready */
+  DebugSetPassthrough();
+  DebugPrintf(au8UserApp1Start1);
+  
+    /* If good initialization, set state to Idle */
   if( 1 )
   {
     UserApp1_StateMachine = UserApp1SM_Idle;
   }
   else
   {
-     /*The task isn't properly initialized, so shut it down and don't run */
+    /* The task isn't properly initialized, so shut it down and don't run */
     UserApp1_StateMachine = UserApp1SM_FailedInit;
-  }   
-    LedOff(BLUE);
-    LedOff(WHITE);
-    LedOff(CYAN);
-    LedOff(GREEN);
-    LedOff(ORANGE);
-    LedOff(RED);
-    LedOff(YELLOW);
-    LedOff(PURPLE);
-    
+  }
 
-  
-      
-      
 } /* end UserApp1Initialize() */
 
   
@@ -128,13 +114,12 @@ Promises:
 void UserApp1RunActiveState(void)
 {
   UserApp1_StateMachine();
+
 } /* end UserApp1RunActiveState */
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-/* Private functions    
-                                                                                              */
-
+/* Private functions                                                                                                  */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 
@@ -143,13 +128,13 @@ State Machine Function Definitions
 **********************************************************************************************************************/
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-/* Wait for ??? */
-
+/* Wait for input */
 static void UserApp1SM_Idle(void)
 {
-
+  
 } /* end UserApp1SM_Idle() */
-    
+                      
+            
 #if 0
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* Handle an error */
